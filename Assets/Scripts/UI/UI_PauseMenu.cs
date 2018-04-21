3f5fce3675627dce46c8a46352de67398bcd14e6
Rebecca <rdodson94@yahoo.co.uk>
2018-04-21 15:24:59 +0100

    rubyrubyrubyruby

--- Assets/Scripts/UI/UI_PauseMenu.cs
+++ Assets/Scripts/UI/UI_PauseMenu.cs
@@ -2,15 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UI_PauseMenu : MonoBehaviour {
-
+public class UI_PauseMenu : MonoBehaviour
+{
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 		
+	}
+
+	public void ExitToMain_OnClick()
+	{
+		if (Core.theCore != null)
+		{
+			Core.theCore.RequestState(Core.CORE_STATE.START_MENU);
+		}
+	}
+
+	public void ExitDesktop_OnClick()
+	{
+		Application.Quit();
 	}
 }
