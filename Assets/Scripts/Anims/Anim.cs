b613ded68b7526920fa616c135ff5b313ea4f9ab
jamioflan <jamioflan@hotmail.co.uk>
2018-04-21 15:09:45 +0100

    Humins

---
+++ Assets/Scripts/Anims/Anim.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Anim : MonoBehaviour
+{
+    public abstract Sprite GetSprite(float fTime);
+}
---
+++ Assets/Scripts/Humans/Human.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Human : Entity
+{
+
+    public float fSpeed = 1.0f;
+
+    public override void Update()
+    {
+        base.Update();
+
+
+
+        transform.localPosition += new Vector3(Time.deltaTime * fSpeed * (bFlip ? -1.0f : 1.0f), 0.0f, 0.0f);
+    }
+}
