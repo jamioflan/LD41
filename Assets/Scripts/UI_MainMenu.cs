911414b301f2e881a8df3b78cfa07fa7d7acb8fe
Rebecca <rdodson94@yahoo.co.uk>
2018-04-21 14:50:50 +0100

    Menus stuff

--- Assets/Scripts/UI_MainMenu.cs
+++ Assets/Scripts/UI_MainMenu.cs
@@ -2,15 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UI_MainMenu : MonoBehaviour {
-
+public class UI_MainMenu : MonoBehaviour
+{
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 		
+	}
+
+	public void Start_OnClick()
+	{
+		if (Core.theCore != null)
+		{
+			Core.theCore.RequestState(Core.CORE_STATE.IN_GAME);
+		}
 	}
 }
