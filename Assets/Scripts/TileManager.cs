1ddfd2539262a24599f2e62af0ad2287a69a8e5c
j0nburr <jonathan.thomas.burr@cern.ch>
2018-04-21 14:31:31 +0100

    Add basic room creation
    Right now just builds a couple of storage rooms at the start

---
+++ Assets/Scripts/TileManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour {
+
+    public static int width = 10;
+    public static int depth = 50;
+
+    public TileBase[] prefabs;
+
+    public TileBase[,] tiles = new TileBase[width, depth];
+
+	// Use this for initialization
+	void Start () {
+        for (int ii = 0; ii < width; ++ii)
+        {
+            for (int jj = 0; jj < depth; ++jj)
+            {
+                if (jj == 0 && (ii == 5 || ii == 6))
+                {
+                    CreateNewTile(ii, jj, TileBase.TileType.STORAGE);
+                }
+                else
+                {
+                    CreateNewTile(ii, jj, TileBase.TileType.EMPTY);
+                }
+            }
+        }
+        		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // Create a new tile
+    TileBase CreateNewTile(int x, int y, TileBase.TileType type)
+    {
+        if (tiles[x, y] != null)
+        {
+            tiles[x, y].Destroy();
+        }
+        tiles[x, y] = Instantiate<TileBase>(prefabs[(int)type]);
+        tiles[x, y].transform.SetParent(transform);
+        tiles[x, y].SetCoords(x, y);
+        return tiles[x, y];
+    }
+
+
+}
---
+++ Assets/Scripts/Tiles/TileBase.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileBase : MonoBehaviour {
+
+    public int x;
+    public int y;
+
+    public enum TileType {
+        EMPTY = 0,
+        STORAGE = 1,
+    }
+        
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public virtual void Destroy()
+    {
+
+    }
+
+
+    public void SetCoords(int px, int py)
+    {
+        x = px;
+        y = py;
+        transform.position = new Vector3(x + 0.5f - TileManager.width / 2, -0.5f - y, -2);
+    }
+
+}
