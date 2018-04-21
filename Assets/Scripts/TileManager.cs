c08df9e83c1ab4260d96ca353aeffb42404404a7
j0nburr <jonathan.thomas.burr@cern.ch>
2018-04-21 17:20:19 +0100

    Pathfinding algorithm complete

--- Assets/Scripts/TileManager.cs
+++ Assets/Scripts/TileManager.cs
@@ -1,6 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
+
+struct Point
+{
+    public int x;
+    public int y;
+    public bool isAccessible;
+    public int travelCost;
+    public int manhattanCost;
+    public static int CoordinateCost(int x, int y)
+    {
+        return TileManager.width * y + x;
+    }
+    // For the open set first sort by the total estimated cost, then by a unique number set by the coordinates
+    public int OpenKey()
+    {
+        return
+            TileManager.width * TileManager.depth * (travelCost + manhattanCost) +
+            Point.CoordinateCost(x, y);
+    }
+    // For the closed set first reverse sort by the travel cost then by a unique number set by the coordinates
+    public int ClosedKey()
+    {
+        return
+            -TileManager.width * TileManager.depth * travelCost +
+            Point.CoordinateCost(x, y);
+    }
+    public int DistanceTo(Point p)
+    {
+        return Math.Abs(x - p.x) + Math.Abs(y - p.y);
+    }
+
+}
+
+struct PointHash : IEqualityComparer<Point>
+{
+    public bool Equals(Point p1, Point p2)
+    {
+        return p1.x == p2.x && p1.y == p2.y;
+    }
+
+    public int GetHashCode(Point p1)
+    {
+        return Point.CoordinateCost(p1.x, p1.y).GetHashCode();
+    }
+}
+
 
 public class TileManager : MonoBehaviour {
 
@@ -59,9 +106,92 @@ public class TileManager : MonoBehaviour {
         return tiles[x, y];
     }
 
+    private Point GetPoint(int x, int y, int currentCost, int targetX, int targetY)
+    {
+        var p = new Point();
+        if (x < 0 || x >= TileManager.width || y < 0 || y >= TileManager.depth)
+        {
+            p.isAccessible = false;
+            return p;
+        }
+        var t = tiles[x, y];    
+        p.x = x;
+        p.y = y;
+        p.isAccessible = t.IsPassable();
+        p.travelCost = currentCost + 1;
+        p.manhattanCost = Math.Abs(x - targetX) + Math.Abs(y - targetY);
+        return p;
+    }
+
+
+
     public List<KeyValuePair<int, int>> GetPath(KeyValuePair<int, int> from, KeyValuePair<int, int> to)
     {
-        var path = new List<KeyValuePair<int, int>>() { from };
+        var considered = new HashSet<Point>(new PointHash());
+        var open = new SortedDictionary<int, Point>();
+        var closed = new SortedDictionary<int, Point>();
+
+        int targetX = to.Key;
+        int targetY = to.Value;
+        Point current = GetPoint(from.Key, from.Value, -1, targetX, targetY);
+        bool doLoop = true;
+        while (doLoop)
+        {
+            
+            // Get everything around the current point
+            var next = new List<Point>() {
+                GetPoint(current.x - 1, current.y, current.travelCost, targetX, targetY),
+                GetPoint(current.x, current.y + 1, current.travelCost, targetX, targetY),
+                GetPoint(current.x + 1, current.y, current.travelCost, targetX, targetY),
+                GetPoint(current.x, current.y - 1, current.travelCost, targetX, targetY),
+            };
+            foreach (Point p in next)
+            {
+                if (!considered.Add(p))
+                    // Returns false if it was already there
+                    continue;
+                if (p.isAccessible)
+                    continue;
+                if (p.x == targetX && p.y == targetY)
+                {
+                    // This means we've reached the target!
+                    doLoop = false;
+                    break;
+                }
+                open.Add(p.OpenKey(), p);
+            }
+            if (!doLoop)
+                break;
+            // Now look at the first 'open' point
+            var nextKey = open.Keys.GetEnumerator().Current;
+            // Add 'current' to closed, remove this one from 'open' and set it to be 'current'
+            closed.Add(current.ClosedKey(), current);
+            current = open[nextKey];
+            open.Remove(nextKey);
+        }
+
+        // Now we can build the path
+        var path = new List<KeyValuePair<int, int>>()
+        {
+            to,
+            new KeyValuePair<int, int>(current.x, current.y)
+        };
+        var itr = closed.GetEnumerator();
+        while(current.travelCost > 0)
+        {
+            // The way we've set up the map it means that they are sorted by travel cost first
+            // This means that we can advance the iterator until it's reached the set of points with the right travel cost
+            int targetKey = - TileManager.width * TileManager.depth * (current.travelCost - 1);
+            while (itr.Current.Key < targetKey)
+                itr.MoveNext();
+            // Now advance until we are next to the 'current' one
+            while (itr.Current.Value.DistanceTo(current) > 1)
+                itr.MoveNext();
+            // We must be next to a new point! Add it to the path and then go again
+            current = itr.Current.Value;
+            path.Add(new KeyValuePair<int, int>(current.x, current.y));
+        }
+
         return path;
     }
 
